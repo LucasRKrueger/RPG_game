a89bb084d6d29e3ddb70ac5ebf8e20fa417e909b
LucasRKrueger <lucasrkthd@gmail.com>
2020-05-23 23:20:17 -0300

    ending the puzzle
    need to implement more puzzles, I did only one yet
    At least 10 for the beta

--- ConsoleRpg/ConsoleRpg/Event.cpp
+++ ConsoleRpg/ConsoleRpg/Event.cpp
@@ -42,29 +42,36 @@ void Event::puzzleEncouter(Character& character)
 	bool completed = false;
 	int userAnswer = 0;
 	int chances = 1;
-	Puzzle puzzle("Puzzles/1.txt");
-
-	while (!completed && chances > 0)
+	int randN = 0;
+	for (size_t i = 1; i < 10; i++)
 	{
-		chances--;
-		std::cout << puzzle.getAsString() + "\n";
+		//IMPLEMENT MORE PUZZLES
+		randN = rand() % 20;
+		Puzzle puzzle("Puzzles/" + std::to_string(randN) + ".txt");
 
-		std::cout << "Your Answer: ";
-		std::cin >> userAnswer;
-		std::cout << "\n";
-		
-		completed = puzzle.getCorrectAnswer() == userAnswer;
-	}
+		while (!completed && chances > 0)
+		{
+			chances--;
+			std::cout << puzzle.getAsString() + "\n";
 
-	if (completed) 
-	{
-		//receive random item
-		character.receiveExperience();
-		std::cout << "CONGRATS YOU SUCCEDED! \n \n";
-	}
-	else
-	{
-		character.stealExperience();
-		std::cout << "YOU FAILED! \n \n";
+			std::cout << "Your Answer: ";
+			std::cin >> userAnswer;
+			std::cout << "\n";
+
+			completed = puzzle.getCorrectAnswer() == userAnswer;
+		}
+
+		if (completed)
+		{
+			//receive random item
+			character.receiveExperience();
+			std::cout << "CONGRATS YOU SUCCEDED! \n \n";
+		}
+		else
+		{
+			//IMPLEMENT WHEN XP BECOMES LESS THAN 0 AND LEVEL BIGGER THAN 1
+			character.stealExperience();
+			std::cout << "YOU FAILED! \n \n";
+		}
 	}
 }
