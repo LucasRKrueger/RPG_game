3c07eaf02abc752151057cbdcc48a3c56903acac
Lucas Krueger <lucasrkthd@gmail.com>
2020-06-04 15:48:57 -0300

    creating shop structure

--- ConsoleRpg/ConsoleRpg/Shop.cpp
+++ ConsoleRpg/ConsoleRpg/Shop.cpp
@@ -1,11 +1,19 @@
 #include "Shop.h"
 Shop::Shop()
 {	
+	this->capacity = 5;
+	this->numberOfItems = 0;
+	this->itemArr = new Item * [capacity];
+	this->initialize();
 }
 
 Shop::~Shop()
 {
-
+	for (size_t i = 0; i < this->numberOfItems; i++)
+	{
+		delete this->itemArr[i];
+	}
+	delete[] itemArr;
 }
 
 vector<Item> Shop::getItems(int characterLevel)
@@ -15,6 +23,61 @@ vector<Item> Shop::getItems(int characterLevel)
 	vector<string>names = setItemNames();
 	for (size_t i = 0; i < 4; i++)
 	{
-		//implement new items
+		items[i] = new Item(names[i], 1, 1, 1, 1);
 	}
+}
+
+Shop::Shop(const Shop& obj)
+{
+	this->capacity = obj.capacity;
+	this->numberOfItems = obj.numberOfItems;
+	this->itemArr = new Item * [this->capacity];
+
+	for (size_t i = 0; i < this->numberOfItems; i++)
+	{
+		this->itemArr[i] = obj.itemArr[i]->clone();
+	}
+	initialize(numberOfItems);
+}
+
+void Shop::addItem(const Item& item)
+{
+	if (this->numberOfItems >= this->capacity)
+	{
+		expand();
+	}
+	this->itemArr[this->numberOfItems++] = item.clone();
+}
+
+void Shop::initialize(const int from)
+{
+	for (size_t i = from; i < capacity; i++)
+	{
+		this->itemArr[i] = nullptr;
+	}
+}
+
+void Shop::expand()
+{
+	this->capacity *= 2;
+
+	Item** tempArr = new Item * [this->capacity];
+
+	for (size_t i = 0; i < this->numberOfItems; i++)
+	{
+		tempArr[i] = this->itemArr[i];
+	}
+
+	delete[] this->itemArr;
+
+	this->itemArr = tempArr;
+
+	this->initialize(this->numberOfItems);
+}
+
+Item& Shop::operator[](const int index)
+{
+	if (index < 0 || index >= this->numberOfItems)
+		throw("BAD INDEX");
+	return *this->itemArr[index];
 }
--- ConsoleRpg/ConsoleRpg/Shop.h
+++ ConsoleRpg/ConsoleRpg/Shop.h
@@ -1,12 +1,30 @@
 #pragma once
 #include "vector"
+#include "Weapon.h"
 #include "Armor.h"
 
-class Shop {
+class Shop{
 public:
 	Shop();
 	virtual ~Shop();
 	vector<Item> getItems(int characterLevel);
 	inline int getRandomItemLevel(int charLevel)const {return rand() % (charLevel+2) - charLevel;}
 	inline vector<string> setItemNames() {return { "Stick","Wooden Shield","Wooden Sword","Wooden Helmet" };}
+
+	Item& operator[](const int index);
+	Shop(const Shop& obj);
+	void addItem(const Item& item);
+	inline void debugPrint() const
+	{
+		for (size_t i = 0; i < this->numberOfItems; i++)
+		{
+			cout << this->itemArr[i]->debugPrint() << endl;
+		}
+	}
+private:
+	int capacity;
+	int numberOfItems;
+	Item** itemArr;
+	void initialize(const int from = 0);
+	void expand();
 };
